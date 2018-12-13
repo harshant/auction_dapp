e8d94759c8f973b9f169f6614c0f62240475a2a2
harshant <harshant.sharma@gmail.com>
2018-12-14 02:10:47 +0530

    Initial working smart contract

--- auction.sol
+++ auction.sol
@@ -4,20 +4,23 @@ pragma solidity ^0.4.25;
 contract Auction{
     
     uint EndingTime;
-    
     address Owner;
-    
     uint HighestBid;
-    
-    uint HighestBidder;
-    
+    address HighestBidder;
+    bool Islive = false;
     mapping (address => uint) BidderAcc;
     
     constructor(uint InitialVal, uint endtime) public{
         Owner = msg.sender;
         EndingTime = endtime;
         HighestBid = InitialVal;
+        if(endtime > now){
+            Islive = true;
+        }
     }
+    
+    event NewHighestBid(address Bidder, uint amount);
+    event AuctionEnded(address HighestBidder, uint HighestBid);
     
     modifier OnlyOwner{
         require(msg.sender == Owner);
@@ -25,16 +28,23 @@ contract Auction{
     }
     
     function Bid(uint amount) payable public{
-        if(amount > HighestBid){
+        if(amount > HighestBid && Islive == true){
             BidderAcc[msg.sender] = amount;
             HighestBid = amount;
             HighestBidder = msg.sender;
+            emit NewHighestBid(HighestBidder, HighestBid);
         }
         else{
-            
+            emit AuctionEnded(HighestBidder,HighestBid);
+        }
+    }
+    
+    function Withdraw() payable public{
+        if(BidderAcc[msg.sender] > 0){
+            BidderAcc[msg.sender] = 0;
+            msg.sender.transfer(BidderAcc[msg.sender]);
         }
         
     }
-    
     
 }
